e8150b7db99d722b4ca23cbbc6ce8697cd708222
yamat0jp <terukohietori@gmail.com>
2020-01-15 13:57:21 +0900

    ツールの変更もコミット　開発休止

--- tool/Unit2.pas
+++ tool/Unit2.pas
@@ -13,7 +13,8 @@ uses
   FireDAC.Comp.DataSet, FireDAC.Comp.Client, System.Rtti,
   System.Bindings.Outputs, Vcl.Bind.Editors, Data.Bind.EngExt,
   Vcl.Bind.DBEngExt, Data.Bind.Components, Data.Bind.DBScope,
-  FireDAC.VCLUI.Wait, FireDAC.Comp.UI, Vcl.DBCtrls, Jpeg, Vcl.Grids, Vcl.DBGrids;
+  FireDAC.VCLUI.Wait, FireDAC.Comp.UI, Vcl.DBCtrls, Jpeg, Vcl.Grids, Vcl.DBGrids,
+  FireDAC.Phys.IB, FireDAC.Phys.IBDef, FireDAC.Phys.FB, FireDAC.Phys.FBDef;
 
 type
   TForm1 = class(TForm)
@@ -33,18 +34,18 @@ type
     FDGUIxWaitCursor1: TFDGUIxWaitCursor;
     DBNavigator1: TDBNavigator;
     DataSource1: TDataSource;
-    FDQuery1: TFDQuery;
     DBGrid1: TDBGrid;
+    FDTable1ID: TIntegerField;
+    FDTable1NAME: TWideStringField;
+    FDTable1SOURCE: TBlobField;
+    Button4: TButton;
+    FDQuery1: TFDQuery;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
   private
     { Private êÈåæ }
-    img: TJpegImage;
-    procedure streamToField(stream: TStream);
-    procedure filenameToField(filename: string);
   public
     { Public êÈåæ }
   end;
@@ -59,83 +60,48 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   i: Integer;
-  t: TStream;
+  s,t: TStream;
 begin
-  FDTable1.CachedUpdates := true;
   for i := 1 to 10 do
   begin
     t := TResourceStream.Create(HInstance, 'Resource_' + i.ToString, RT_RCDATA);
     FDTable1.AppendRecord([i,Format('slide%d.jpg',[i]),nil]);
-    streamToField(t);
+    FDTable1.Edit;
+    s:=FDTable1.CreateBlobStream(FDTable1.FieldByName('source'),bmWrite);
+    s.CopyFrom(t,0);
+    FDTable1.Post;
+    s.Free;
     t.Free;
   end;
-  FDTable1.ApplyUpdates;
-  FDTable1.CommitUpdates;
-  FDTable1.CachedUpdates := false;
-end;
-
-procedure TForm1.Button2Click(Sender: TObject);
-begin
-  if OpenPictureDialog1.Execute = true then
-    filenameToField(OpenPictureDialog1.FileName);
 end;
 
 procedure TForm1.Button3Click(Sender: TObject);
 var
   s: TStream;
+  j: TJpegImage;
 begin
   s:=FDTable1.CreateBlobStream(FDTable1.FieldByName('source'),bmRead);
-  try
-    img.LoadFromStream(s);
-    Image1.Picture.Assign(img);
-    img.CleanupInstance;
-  finally
-    s.Free;
-  end;
+  j:=TJpegImage.Create;
+  s.Position:=0;
+  j.LoadFromStream(s);
+  s.Free;
+  Image1.Picture.Assign(j);
+  j.Free;
 end;
 
-procedure TForm1.filenameToField(filename: string);
-var
-  field: TStream;
+procedure TForm1.Button4Click(Sender: TObject);
 begin
-  FDTable1.Edit;
-  field:=FDTable1.CreateBlobStream(FDTable1.FieldByName('source'),bmWrite);
-  try
-    img.LoadFromFile(filename);
-    img.SaveToStream(field);
-    FDTable1.Post;
-    img.CleanupInstance;
-  finally
-    field.Free;
-  end;
+  FDTable1.Close;
+  FDQuery1.ExecSQL;
+  FDTable1.CreateTable;
+  FDTable1.Open;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  FDQuery1.ExecSQL;
+  if FDTable1.Exists = false then
+    FDTable1.CreateTable;
   FDTable1.Open;
-  img := TJpegImage.Create;
-end;
-
-procedure TForm1.FormDestroy(Sender: TObject);
-begin
-  img.Free;
-end;
-
-procedure TForm1.streamToField(stream: TStream);
-var
-  field: TStream;
-begin
-  FDTable1.Edit;
-  field := FDTable1.CreateBlobStream(FDTable1.FieldByName('source'), bmWrite);
-  try
-    img.LoadFromStream(stream);
-    img.SaveToStream(field);
-    FDTable1.Post;
-    img.CleanupInstance;
-  finally
-    field.Free;
-  end;
 end;
 
 end.
